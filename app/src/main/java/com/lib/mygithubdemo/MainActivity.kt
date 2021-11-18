18953b56e7894727d34eaa0fc27bdc250ded38bc
vanshikasaini <vanshikasaini301@gmail.com>
2021-11-18 16:20:44 +0530

    add first

--- app/src/main/java/com/lib/mygithubdemo/MainActivity.kt
+++ app/src/main/java/com/lib/mygithubdemo/MainActivity.kt
@@ -2,10 +2,12 @@ package com.lib.mygithubdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("test one","this is what new branch")
     }
 }
